3e21e1ec52275496d4f3481519724948e472b172
Krishk28 <Krish.Kumar2163@gmail.com>
2023-05-25 12:05:47 +1200

    Added entry boxes and spin boxes and then placing said boxes onto the frame using grid

--- Assesment.py
+++ Assesment.py
@@ -12,3 +12,27 @@ frame = tkinter.Frame(window)
 frame.pack()
 party_hire_frame = tkinter.LabelFrame(frame, text="Julie's Party Hire", font = ("",20))
 party_hire_frame.grid(row= 0, column= 0, pady=20, padx=20)
+
+#Labels and headings 
+customer_fullname_label = tkinter.Label(party_hire_frame,text="Customer Full Name")
+customer_fullname_label.grid(row=1,column=0)
+
+Receipt_label = tkinter.Label(party_hire_frame, text = "Receipt Number")
+Receipt_label.grid(row=1,column=1)
+
+#adding entry boxes and spin boxes 
+group_leader_entry = tkinter.Entry(party_hire_frame)
+Receipt_label_entry = tkinter.Entry(party_hire_frame)
+
+#placing said boxes onto frame using grid
+group_leader_entry.grid(row=3,column=0)
+Receipt_label_entry.grid(row=3,column=1)
+
+
+
+
+
+
+
+#loop  
+window.mainloop()
